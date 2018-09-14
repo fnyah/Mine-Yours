90bc078a8be1fdce8989c4c6d7fbe66909a6de56
Beth.BootCamp <beth.gmariam@gmail.com>
2018-09-14 12:58:41 -0400

    routes added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,59 @@
-var db = require("../models");
-
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+// find all Books
+app.get("/api/books", function(req, res) {
+  db.Books.findAll({}).then(function(dbBooks) {
+    res.json(dbBooks);
   });
+});
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+// find all books by id
+app.get("/api/books/:id", function(req, res) {
+  db.Books.findOne({ where: { id: req.params.id } }).then(function(dbBooks) {
+    res.json(dbBooks);
   });
+});
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+// find all Sellers
+app.get("/api/sellers", function(req, res) {
+  db.Sellers.findAll({}).then(function(dbBooks) {
+    res.json(dbBooks);
   });
-};
+});
+
+// find all sellers by id
+app.get("/api/sellers/:id", function(req, res) {
+  db.Sellers.findOne({ where: { id: req.params.id } }).then(function(dbBooks) {
+    res.json(dbBooks);
+  });
+});
+
+//Delete Books by id
+app.delete("/api/books/delete/:id", function(req, res) {
+  db.Books.destroy({ where: { id: req.params.id } }).then(function(dbBooks) {
+    res.json(dbBooks);
+  });
+});
+
+//=================post methods (api)======================
+
+// add new book to database get input from seller form req.body
+app.post("/api/books", function(req, res) {
+  console.log(req.body);
+  db.Books.create({
+    title: req.body.title,
+    description: req.body.description,
+    imageURL: req.body.imageURL,
+    authors: req.body.authors,
+    price: req.body.price
+  }).then(function(dbBooks) {
+    res.json(dbBooks);
+  });
+});
+
+// login?????? check with team
+app.post("/api/login", function(req, res) {
+  // check email from database if user exists...
+  // store user info to database if new user
+  // take to seller page if seller
+  // process order if buyer
+  // update database??
+});
