d644cf0b43124eb6b13b0048ace3d1e9ce88d7cc
fnyah <zackbennett@live.ca>
2018-09-18 22:41:00 -0400

    testing

--- models/index.js
+++ models/index.js
@@ -8,16 +8,18 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize("exampledb", "localhost", "root");
-} else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+var sequelize = new Sequelize("exampledb", "localhost", "root");
+
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//   var sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
 
 fs.readdirSync(__dirname)
   .filter(function(file) {
